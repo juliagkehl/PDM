630edacfac1f86355da27462a19b21146528673e
Julia <kehljulis08@gmail.com>
2026-09-25 11:14:33 -0300

    imc

--- app/src/main/java/com/example/appjulia/MainActivity.java
+++ app/src/main/java/com/example/appjulia/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appjulia;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,32 +12,62 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    int  posicao = 0;
-    @Override
 
+    int posicao = 0;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(
+                    systemBars.left,
+                    systemBars.top,
+                    systemBars.right,
+                    systemBars.bottom
+            );
             return insets;
         });
 
         Integer[] imagens = new Integer[]{
                 R.drawable.perfil,
-                R.drawable.abaixopeso};
-        //colocar todos
+                R.drawable.abaixopeso,
+                R.drawable.normal,
+                R.drawable.obesidade1,
+                R.drawable.obesidade2,
+                R.drawable.obesidade3
+        };
+
         Button botaoVoltar, botaoProximo;
+
         botaoVoltar = findViewById(R.id.btnVoltar);
         botaoProximo = findViewById(R.id.btnProximo);
+
         ImageView imageView = findViewById(R.id.imageView);
 
-        botaoProximo.setOnClickListener( View -> {
-            posicao ++;
-            imageView.setImageResource(Imagens[posicao]);
+        // Botão Próximo
+        botaoProximo.setOnClickListener(view -> {
+            posicao++;
+
+            if (posicao >= imagens.length) {
+                posicao = 0;
+            }
+
+            imageView.setImageResource(imagens[posicao]);
         });
 
+        // Botão Voltar
+        botaoVoltar.setOnClickListener(view -> {
+            posicao--;
+
+            if (posicao < 0) {
+                posicao = imagens.length - 1;
+            }
+
+            imageView.setImageResource(imagens[posicao]);
+        });
     }
 }
